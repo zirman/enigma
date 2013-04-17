e74abb6864c252354e549fe9181da45d5661a0bd
Tyrone Traynum <tyrtra7148@students.ecpi.edu>
2013-04-16 21:38:52 -0400

    This is the Main.js Controller unit tests. All 9 tests have passed.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,5 +10,29 @@ strict: true
   'use strict';
 
   ENIGMA.EnigmaController = function () {
+    var enigmaMachine;
+    enigmaMachine = new ENIGMA.EnigmaMachine();
+
+    var enigmaView;
+    enigmaView = new ENIGMA.EnigmaView();
+
+
+
+
+    this.getEnigmaMachine = function () {
+      return enigmaMachine;
+    };
+
+    this.getEnigmaView = function () {
+      return enigmaView;
+    };
+
+    this.keyboardEvent = function () {
+
+    };
+
+    this.setEnigmaView = function (newEnigmaView) {
+      enigmaView = newEnigmaView;
+    };
   };
 }());

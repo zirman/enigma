881c08eb05d1dc7aec8799b361cc19c7987b3d4b
Tyrone Traynum <tyrtra7148@students.ecpi.edu>
2013-04-25 17:56:02 -0400

    This is the codeBook.js file. This is the Code Book for the ENIGMA MACHINE.

--- app/scripts/codeBook.js
+++ app/scripts/codeBook.js
@@ -1,80 +1,82 @@
+/*jshint quotmark: double*/
+//JSON requires doublequotes on every string
 {
-  codeBook: [
+  "codeBook": [
     {
-      rotorConfig: ['I', 'III', 'V'];
-      keyRingSetting: [8, 23, 24];
-      plugBoardConfig: ['CQ', 'RZ', 'IS', 'TU', 'VW',
-                        'XY', 'ZA', 'BJ', 'DE', 'FG',
-                        'HI', 'KL', 'MN', 'OP'];
-      keyIdentifier: ['TEU'];
+      "rotorConfig": ["I", "III", "V"],
+      "keyRingSetting": [8, 23, 24],
+      "plugBoardConfig": ["CQ", "RZ", "IS", "TU", "VW",
+                        "XY", "ZA", "BJ", "DE", "FG",
+                        "HI", "KL", "MN", "OP"],
+      "keyIdentifier": ["TEU"]
     },
     {
-      rotorConfig: ['II', 'IV', 'I'];
-      keyRingSetting: [25, 13, 1];
-      plugBoardConfig: ['AB', 'CD', 'EF', 'GH', 'IJ',
-                        'KL', 'MN', 'QR', 'ST', 'UV',
-                        'WX', 'YZ'];
-      keyIdentifier: ['PZA'];
+      "rotorConfig": ["II", "IV", "I"],
+      "keyRingSetting": [25, 13, 1],
+      "plugBoardConfig": ["AB", "CD", "EF", "GH", "IJ",
+                        "KL", "MN", "QR", "ST", "UV",
+                        "WX", "YZ"],
+      "keyIdentifier": ["PZA"]
     },
     {
-      rotorConfig: ['III', 'V', 'II'];
-      keyRingSetting: [9, 23, 14];
-      plugBoardConfig: ['AZ', 'YB', 'CX', 'DW', 'EV',
-                        'FU', 'GT', 'HS', 'IR', 'JQ',
-                        'KP', 'LO', 'NM'];
-      keyIdentifier: ['YAK'];
+      "rotorConfig": ["III", "V", "II"],
+      "keyRingSetting": [9, 23, 14],
+      "plugBoardConfig": ["AZ", "YB", "CX", "DW", "EV",
+                        "FU", "GT", "HS", "IR", "JQ",
+                        "KP", "LO", "NM"],
+      "keyIdentifier": ["YAK"]
     },
     {
-      rotorConfig: ['IV', 'I', 'III'];
-      keyRingSetting: [10, 20, 2];
-      plugBoardConfig: ['AC', 'BD', 'EG', 'FH', 'IK',
-                        'JL', 'MO', 'NP', 'QT', 'RS',
-                        'UW', 'VX', 'YZ'];
-      keyIdentifier: ['RAY'];
+      "rotorConfig": ["IV", "I", "III"],
+      "keyRingSetting": [10, 20, 2],
+      "plugBoardConfig": ["AC", "BD", "EG", "FH", "IK",
+                        "JL", "MO", "NP", "QT", "RS",
+                        "UW", "VX", "YZ"],
+      "keyIdentifier": ["RAY"]
     },
     {
-      rotorConfig: ['V', 'II', 'IV'];
-      keyRingSetting: [2, 12, 21];
-      plugBoardConfig: ['RC', 'IS', 'TE', 'GD', 'VJ',
-                        'OK', 'ZP'];
-      keyIdentifier: ['SAP'];
+      "rotorConfig": ["V", "II", "IV"],
+      "keyRingSetting": [2, 12, 21],
+      "plugBoardConfig": ["RC", "IS", "TE", "GD", "VJ",
+                        "OK", "ZP"],
+      "keyIdentifier": ["SAP"]
     },
     {
-      rotorConfig: ['I', 'II', 'III'];
-      keyRingSetting: [7, 5, 8];
-      plugBoardConfig: ['EU', 'AT', 'PG', 'BL', 'JH',
-                        'ZS', 'RF', 'DK', 'CI', 'MQ',
-                        'NP', 'OY', 'XW', 'VZ'];
-      keyIdentifier: ['MUC'];
+      "rotorConfig": ["I", "II", "III"],
+      "keyRingSetting": [7, 5, 8],
+      "plugBoardConfig": ["EU", "AT", "PG", "BL", "JH",
+                        "ZS", "RF", "DK", "CI", "MQ",
+                        "NP", "OY", "XW", "VZ"],
+      "keyIdentifier": ["MUC"]
     },
     {
-      rotorConfig: ['II', 'III', 'IV'];
-      keyRingSetting: [13, 23, 3];
-      plugBoardConfig: ['TA', 'EZ', 'UO', 'IK', 'PR',
-                        'YD'];
-      keyIdentifier: ['QRZ'];
+      "rotorConfig": ["II", "III", "IV"],
+      "keyRingSetting": [13, 23, 3],
+      "plugBoardConfig": ["TA", "EZ", "UO", "IK", "PR",
+                        "YD"],
+      "keyIdentifier": ["QRZ"]
     },
     {
-      rotorConfig: ['III', 'IV', 'V'];
-      keyRingSetting: [4, 14, 24];
-      plugBoardConfig: ['ED', 'NO', 'TZ', 'AB', 'RU',
-                        'MY', 'IL'];
-      keyIdentifier: ['NUT'];
+      "rotorConfig": ["III", "IV", "V"],
+      "keyRingSetting": [4, 14, 24],
+      "plugBoardConfig": ["ED", "NO", "TZ", "AB", "RU",
+                        "MY", "IL"],
+      "keyIdentifier": ["NUT"]
     },
     {
-      rotorConfig: ['IV', 'V', 'I'];
-      keyRingSetting: [16, 1, 15];
-      plugBoardConfig: ['PA', 'BE', 'DO', 'KC', 'JR',
-                        'IT', 'LM', 'NV', 'FS', 'GZ',
-                        'YH', 'XQ', 'WU'];
-      keyIdentifier: ['WRD'];
+      "rotorConfig": ["IV", "V", "I"],
+      "keyRingSetting": [16, 1, 15],
+      "plugBoardConfig": ["PA", "BE", "DO", "KC", "JR",
+                        "IT", "LM", "NV", "FS", "GZ",
+                        "YH", "XQ", "WU"],
+      "keyIdentifier": ["WRD"]
     },
     {
-      rotorConfig: ['V', 'I', 'II'];
-      keyRingSetting: [11, 22, 9];
-      plugBoardConfig: ['BI', 'DT', 'UG', 'QA', 'ZE',
-                        'WS', 'VO'];
-      keyIdentifier: ['FIG'];
+      "rotorConfig": ["V", "I", "II"],
+      "keyRingSetting": [11, 22, 9],
+      "plugBoardConfig": ["BI", "DT", "UG", "QA", "ZE",
+                        "WS", "VO"],
+      "keyIdentifier": ["FIG"]
     }
   ]
 }

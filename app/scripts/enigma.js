1b072b7535db1e44e4a0aa6b540a201cf501ab6a
Ingus Smith <ingus.smith@gmail.com>
2013-05-05 15:36:37 -0400

    Add comments and documentation to the enigma.js file

--- app/scripts/enigma.js
+++ app/scripts/enigma.js
@@ -6,12 +6,32 @@ strict: true
 
 /*globals ENIGMA*/
 
+
+/**
+ * Immediate function that instantiates Enigma class.
+ */
 (function () {
   'use strict';
 
   var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
     'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
+  /**
+   * Creates the Enigma machine container.
+   * @class
+   * @classdesc Create the Enigma interface and the machine object.
+   *
+   * @property {Rotor} reflector                    -  Object wiring the rotor
+   * to reflector.
+   * @property {Reflector|MiddleRotor} leftRotor    -  Object wiring the middle
+   * rotor and the reflector.
+   * @property {Rotor} middleRotor                  -  Object wiring the left
+   * and right rotor.
+   * @property {Rotor|Plugboard} rightRotor         -  Object wiring the middle
+   * and the plugboard.
+   * @property {Rotor} plugboard                    -  Object wiring the right
+   * rotor
+   */
   ENIGMA.EnigmaMachine = function () {
     // create all plugboard, rotors and reflector
     var reflector = new ENIGMA.Reflector.ReflectorB();
@@ -36,6 +56,22 @@ strict: true
 
     plugboard.setLeftObj(rightRotor);
 
+    /**
+     * @namespace
+     * @property {object} middleRotor      - Middle text field in the ground
+     * setting
+     * @property {object} leftRotor        - Left text field in the ground
+     * setting
+     * @property {object} rightRotor       - Right text field in the ground
+     * setting
+     * If the middleRotor has made a full cycle, the leftRotor will rotate one
+     * letter.
+     * If the rightRotor has made a full cycle, the middleRotor will rotate one
+     * letter; or if the middleRotor has advanced one letter from the rightRotor
+     * already the middleRotor will rotate one letter again.
+     * The rightRotor will cycle through the letters with each key stroke from
+     * the user.
+     */
     var advanceRotors = function () {
       if (middleRotor.isOnNotch()) {
         leftRotor.advanceGroundSetting();
@@ -49,6 +85,20 @@ strict: true
       rightRotor.advanceGroundSetting();
     };
 
+    /**
+     * Returns the configuration for the reflector setting, rotors setting,
+     * ring setting, ground setting, and plugboard setting; which will display
+     * in the users browser.
+     * @return {string} Gather reflector, rotor, ring, ground, and plugboard
+     * settings and displays in the browser.
+     * @property {object} middleRotor      - Middle text field in the ground
+     * setting
+     * @property {object} leftRotor        - Left text field in the ground
+     * setting
+     * @property {object} rightRotor       - Right text field in the ground
+     * setting
+     * @property {Rotor} plugboard         -  Object wiring the right rotor
+     */
     this.getAllSettings = function () {
       var reflectorConfig = reflector.getLabel();
 
@@ -80,6 +130,10 @@ strict: true
         groundSettings + plugboardSettings;
     };
 
+    /**
+     * Sets the Enigma Machine's settings property.
+     * @param {Reflector|Rotor|Ring|Ground|Plugboard} settings
+     */
     this.setAllSettings = function (settings) {
       var items = settings.toUpperCase().split('-');
       var reflectorLabel = items[0];
@@ -144,30 +198,54 @@ strict: true
       });
     };
 
+    /**
+     * Returns this plugboard property.
+     * @return {object} plugboard
+     */
     this.getPlugboard = function () {
       return plugboard;
     }.bind(this);
 
+    /**
+     * Return this leftRotor property.
+     * @return {object} leftRotor
+     */
     this.getLeftRotor = function () {
       return leftRotor;
     }.bind(this);
 
+    /**
+     * Return this middleRotor property.
+     * @return {object} middleRotor
+     */
     this.getMiddleRotor = function () {
       return middleRotor;
     }.bind(this);
 
+    /**
+     * Return this rightRotor property.
+     * @return {object} rightRotor
+     */
     this.getRightRotor = function () {
       return rightRotor;
     }.bind(this);
 
+    /**
+     * Return this Reflector property.
+     * @return {object} Reflector
+     */
     this.getReflector = function () {
       return reflector;
     }.bind(this);
 
-    // takes oldRotor and replaces it with newRotor
-    // example usage:
-    // enigmaMachine.swapRotor(enigmaMachine.getLeftRotor(), rotor7);
-    // this puts rotor 7 into the left rotor slot
+    /**
+     * takes oldRotor and replaces it with newRotor
+     * example usage:
+     * enigmaMachine.swapRotor(enigmaMachine.getLeftRotor(), rotor7);
+     * this puts rotor 7 into the left rotor slot
+     * @param {object} oldRotor
+     * @param {object} newRotor
+     */
     this.swapRotor = function (oldRotor, newRotor) {
       console.assert(oldRotor === leftRotor || oldRotor === middleRotor ||
         oldRotor === rightRotor);
@@ -191,19 +269,26 @@ strict: true
       }
     }.bind(this);
 
-    // swaps reflector out with newReflector
-    // example usage:
-    // enigmaMachine.swapReflector(refelctorC);
-    // this puts reflectorC into the reflector slot
+    /**
+     * swaps reflector out with newReflector
+     * example usage:
+     * enigmaMachine.swapReflector(refelctorC);
+     * this puts reflectorC into the reflector slot
+     * @param {object} newReflector
+     */
     this.swapReflector = function (newReflector) {
       newReflector.setRightObj(leftRotor);
       leftRotor.setLeftObj(newReflector);
       reflector = newReflector;
     }.bind(this);
 
-    // enciphers a single character
-    // only the first character of the string is enciphered and returned
-    // non a-z characters are passed through in lower case
+    /**
+     * enciphers a single character
+     * only the first character of the string is enciphered and returned
+     * non a-z characters are passed through in lower case
+     * @param {string} letter
+     * @return {string} Upper case letter
+     */
     this.encipherLetter = function (letter) {
       var upperCase = letter.toUpperCase();
       var charCode = upperCase.charCodeAt(0);
@@ -218,6 +303,13 @@ strict: true
       return plugboard.goingLeft(upperCase);
     }.bind(this);
 
+    /**
+     * Traces a single character
+     * only the first character of the string is enciphered and returned
+     * non a-z characters are passed through in lower case
+     * @param {string} letter
+     * @return {string} Upper case letter
+     */
     this.traceLetter = function (letter) {
       var upperCase = letter.toUpperCase();
       var charCode = upperCase.charCodeAt(0);
@@ -237,8 +329,12 @@ strict: true
       };
     }.bind(this);
 
-    // enciphers a string
-    // non a-z characters are passed through in lower case
+    /**
+     * enciphers a string
+     * non a-z characters are passed through in lower case
+     * @param {string} cleartext
+     * @return {string} Encipher string
+     */
     this.encipherString = function (cleartext) {
       var ciphertext = '';
 
